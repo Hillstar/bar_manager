f725d230d5a5326ec3a64c5fda098d87f9dbbe47
Ivan <krioldar@gmail.com>
2023-12-02 21:13:33 +0700

    Added reading from json by button and some refactoring

--- Bar Manager/Bar Manager/ContentView.swift
+++ Bar Manager/Bar Manager/ContentView.swift
@@ -7,63 +7,81 @@
 
 import SwiftUI
 
-struct Coctailtem: Identifiable {
-    let id = UUID()
+struct Coctailtem: Identifiable, Decodable {
+    let id = UUID().uuidString
     let name: String
     let imageName: String
     let ingredients: String
     let recipe: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case imageName
+        case ingredients
+        case recipe
+    }
 }
 
 struct CoctailView: View {
     let coctail: Coctailtem
     
     var body: some View {
-        VStack {
-            Text(coctail.name)
-                .fontWeight(.bold)
-                .multilineTextAlignment(.center)
-            Image(coctail.imageName).resizable()
-                .cornerRadius(15)
-                .aspectRatio( contentMode: .fit)
-                .padding(.all)
-            Text(coctail.ingredients)
-            Text(coctail.recipe)
+        VStack(spacing: 10) {
+            ScrollView {
+                Text(coctail.name)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 40))
+                Image(coctail.imageName)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 350, height: 350)
+                    .clipped()
+                    .cornerRadius(15)
+                    .padding(.all)
+                Text("INGREDIENTS: ")
+                    .fontWeight(.bold)
+                Text(coctail.ingredients)
+                    .padding(.horizontal, 20)
+                    .multilineTextAlignment(.leading)
+                Text("RECIPE: ")
+                    .fontWeight(.bold)
+                Text(coctail.recipe)
+                    .multilineTextAlignment(.leading)
+                    .padding(.horizontal, 20)
+            }
         }
     }
 }
 
 struct ContentView: View {
-    let coctails = [
-        Coctailtem(
-            name: "Daiquiri",
-            imageName: "daiquiri.original",
-            ingredients: "1 1/2 oz White rum, 1 oz Lime juice, 1/2 oz Simple syrup",
-            recipe: "Shake all ingredients in a shaker"
-            ),
-        Coctailtem(
-            name: "Old fashioned",
-            imageName: "old_fashioned.original",
-            ingredients: "1 1/2 oz Bourbon or Rye whiskey, 1 Sugar cube, 2 dashes Angostura bitters, Few dashes plain water",
-            recipe: "Place sugar cube in old fashioned glass and saturate with bitters, add a dash of plain water. Muddle until dissolved. Fill the glass with ice cubes and add whiskey. Garnish with orange slice, and a cocktail cherry"
-            ),
-        Coctailtem(
-            name: "Aperol Spritz",
-            imageName: "aperol_spritz.original",
-            ingredients: "1 1/4 oz Aperol, 2 oz Prosecco, Splash of Soda water",
-            recipe: "Build into glass over ice, garnish and serve"
-            ),
-    ]
+    @State var coctails_loaded:[Coctailtem] = []
+    
     var body: some View {
-        NavigationView {
-            List(coctails) { coctail in
-                NavigationLink( destination: {
-                    CoctailView(coctail: coctail)
-                }, label: {
-                    Text(coctail.name)
+        VStack {
+            Button("Get dafault value") {
+                guard let path = Bundle.main.url(forResource: "default_coctails", withExtension: "json") else {
+                    return
                 }
-                )
-                .navigationTitle("Coctails")
+                do {
+                    let data = try Data(contentsOf: path)
+                    coctails_loaded = try JSONDecoder().decode([Coctailtem].self, from: data)
+                } catch {
+                    print(error)
+                }
+            }
+            Spacer()
+            NavigationView {
+                List(coctails_loaded) { coctail in
+                    NavigationLink(
+                        destination: {
+                            CoctailView(coctail: coctail)
+                        }, label: {
+                            Text(coctail.name)
+                        }
+                    )
+                    .navigationTitle("Coctails")
+                }
             }
         }
     }
